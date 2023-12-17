6affdb8c050fe55b757a48cf269a9c326ddc16fd
redennessy14 <136608186+redennessy14@users.noreply.github.com>
2023-12-17 14:59:42 +0600

    edit loading screen x2

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -5,29 +5,8 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Model = () => {
-  const [loadingPercentage, setLoadingPercentage] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
   const { scene } = useGLTF("/model/prog/scene.gltf");
-  console.log(loadingPercentage, "load");
   const group = useRef();
-
-  useEffect(() => {
-    const loader = new GLTFLoader();
-    loader.load(
-      "/model/prog/scene.gltf",
-      (gltf) => {
-        scene.add(gltf.scene);
-        setIsLoading(false); // Установка isLoading в false после завершения загрузки
-      },
-      (xhr) => {
-        const percentLoaded = Math.round((xhr.loaded / xhr.total) * 100);
-        setLoadingPercentage(percentLoaded);
-      },
-      (error) => {
-        console.error("An error happened", error);
-      }
-    );
-  }, [scene]);
 
   useFrame(({ mouse }) => {
     const sensitivity = 0.003;
@@ -46,23 +25,16 @@ const Model = () => {
   });
 
   return (
-    <>
-      {isLoading ? (
-        <Text style={{ position: "absolute", top: 10, left: 10 }}>
-          Loading: {loadingPercentage}%
-        </Text>
-      ) : null}
-      {!isLoading && scene && (
-        <group ref={group}>
-          <primitive
-            position={[0, 0, 0]}
-            object={scene}
-            scale={[1.5, 1.5, 1.5]}
-            rotation={[5, 0, 5]}
-          />
-        </group>
+    <group ref={group}>
+      {scene && (
+        <primitive
+          position={[0, 0, 0]}
+          object={scene}
+          scale={[1.5, 1.5, 1.5]}
+          rotation={[5, 0, 5]}
+        />
       )}
-    </>
+    </group>
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,28 +5,35 @@ import AnimatedModel from "../../components/AnimatedModel/AnimatedModel";
 import NameBlock from "../../components/NameBlock/NameBlock";
 
 const HomePage = () => {
-  // const [loadingProgress, setLoadingProgress] = useState(0);
-  // const [isLoading, setIsLoading] = useState(true);
+  const [loadingProgress, setLoadingProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const loadingInterval = setInterval(() => {
-  //     setLoadingProgress((prevProgress) => {
-  //       if (prevProgress >= 100) {
-  //         setIsLoading(false);
-  //         clearInterval(loadingInterval);
-  //         return 100;
-  //       } else {
-  //         return prevProgress + 10;
-  //       }
-  //     });
-  //   }, 500);
+  useEffect(() => {
+    const hasVisited = localStorage.getItem("hasVisited");
 
-  //   return () => clearInterval(loadingInterval);
-  // }, []);
+    if (!hasVisited) {
+      const loadingInterval = setInterval(() => {
+        setLoadingProgress((prevProgress) => {
+          if (prevProgress >= 100) {
+            setIsLoading(false);
+            clearInterval(loadingInterval);
+            localStorage.setItem("hasVisited", "true");
+            return 100;
+          } else {
+            return prevProgress + 10;
+          }
+        });
+      }, 500);
 
-  // if (isLoading) {
-  //   return <div className="loading">Loading... {loadingProgress}%</div>;
-  // }
+      return () => clearInterval(loadingInterval);
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
+
+  if (isLoading) {
+    return <div className="loading">Loading... {loadingProgress}%</div>;
+  }
 
   return (
     <div className="homepage">
