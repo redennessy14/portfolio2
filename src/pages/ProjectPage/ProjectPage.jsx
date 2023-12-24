e5bc8ef33d191086f137c562e81acbacff5484f0
redennessy14 <136608186+redennessy14@users.noreply.github.com>
2023-12-24 20:40:23 +0600

    adaptive update x4

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import "./ProjectPage.css";
 import IMG1 from "../../images/starbucks.png";
+import VID1 from "../../video/portfolio.MOV";
 
 const ProjectPage = () => {
   return (
     <div className="project">
       <h2>My Projects</h2>
       <div className="project__block">
+        {" "}
+        <video controls autoPlay className="project__block_img">
+          <source src={VID1} type="video/mp4" />
+        </video>
         <div>
-          {" "}
           <h3 className="project__block_name">Portfolio</h3>
           <p className="project__block_desc">JS , HTML , CSS , Three Js</p>
         </div>
